9522f0b77aa0c086582e0f653554c61d3dc45fa3
Kristian Mariyanov <kmariyan@progress.com>
2025-09-12 14:49:55 +0300

    chore: fix config in vite; improve publish workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/nuclea-demo/' : '/',
+  base: '/',
+  // TODO: enable when deploying to public GitHub Pages,
+  // base: command === 'build' ? '/nuclea-demo/' : '/',
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': command === 'build' ? 'http://localhost:5000' : 'https://nuclea-kendo-demo.azurewebsites.net/',
     }
   }
 }))
