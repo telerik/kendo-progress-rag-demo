e157a8d3b6a95886c80c0c17bc8e5270b551fbbf
Kristian Mariyanov <kmariyan@progress.com>
2025-09-20 14:47:55 +0300

    chore: update base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
   plugins: [react()],
-  base: '/nuclia-demo/',
+  base: '/kendo-progress-rag-demo/',
   server: {
     port: 5173,
     proxy: {
